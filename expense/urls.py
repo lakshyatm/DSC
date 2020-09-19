7a3710c27d0fa2ad2d94b41a856b243169729a92
lakshyatm <58063296+lakshyatm@users.noreply.github.com>
2020-09-19 12:08:54 +0530

    Update urls.py

--- expense/urls.py
+++ expense/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('second/',views.second,name="second"),
     path('second/third/',views.third,name="third"),
+    path('second/third/final/',views.fourth,name="fourth"),
 ]
